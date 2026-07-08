67f8d8306477a3e24bc1ccb00e4b348b92079320
Ethan <ethanhowes06@gmail.com>
2026-07-08 01:29:23 -0400

    bugfix: added include time and return line for errors

--- cuda/terrain/main.c
+++ cuda/terrain/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "include/io.h"
 #include "include/kernels.h"
@@ -37,6 +38,7 @@ int main(int argc, char *argv[]) {
 
     if (!slope || !aspect || !plan || !profile || !tri) {
 	fprintf(stderr, "Error: failed to allocate output arrays \n");
+	return 1;
     }
 
     // kernel launching and timing
@@ -94,14 +96,32 @@ int main(int argc, char *argv[]) {
     cudaEventDestroy(gpu_start);
     cudaEventDestroy(gpu_stop);
 
+
+    // save output rasters
+    WriteDEM(slope, dem.valuesRead, slope_path);
+    WriteDEM(aspect, dem.valuesRead, aspect_path);
+    WriteDEM(plan, dem.valuesRead, plan_path);
+    WriteDEM(profile, dem.valuesRead, profile_path);
+    WriteDEM(tri, dem.valuesRead, tri_path);
+
+    
+    printf("gpu_time_ms_slope_aspect=%f\n", gpu_time_ms_slope_aspect);
+    printf("cpu_time_ms_slope_aspect=%f\n", cpu_time_ms_slope_aspect);
+
+    printf("gpu_time_ms_curvature=%f\n", gpu_time_ms_curvature);
+    printf("cpu_time_ms_curvature=%f\n", cpu_time_ms_curvature);
+
+    printf("gpu_time_ms_tri=%f\n", gpu_time_ms_tri);
+    printf("cpu_time_ms_tri=%f\n", cpu_time_ms_tri);
+
+    free(slope);
+    free(aspect);
+    free(plan);
+    free(profile);
+    free(tri);
+
+    FreeRasterData(&dem);
+    return 0;
+
 }
-
-
-
-
-
-
-
-
-
 
